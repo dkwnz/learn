63d356a18d20b1a3be83fdb43c08a8f0b4600e32
ningning <dnnsmile@163.com>
2018-01-10 15:56:33 +0800

    add test

--- skl.py
+++ skl.py
@@ -3,23 +3,44 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn import metrics
 from sklearn import cross_validation
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
 
 #读取数据，以逗号隔开的csv文件
-dataset = pd.read_csv('dataset.csv', header=None, sep=',')
+#选择文件
+filename = input('输入训练数据文件')
+dataset = pd.read_csv(filename, header=None, sep=',')
 dataset = np.array(dataset)
 X_len = len(dataset[0]) - 1
 X = dataset[:,:X_len]
 y = dataset[:,X_len:].reshape(-1,)
 #标准化
-#normalized_X = preprocessing.normalize(X)
-#数据分割
-from sklearn import cross_validation
+scaler = preprocessing.StandardScaler().fit(X)
+X = scaler.transform(X)
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.1)
+
 #选择算法
-clf = SCV()
-#训练
+clf_logreg = LogisticRegression()
+clf_nvby = GaussianNB()
+clf_knn = KNeighborsClassifier()
+clf_tree = DecisionTreeClassifier()
+clf_svm = SVC()
+clf = clf_tree
 clf.fit(X_train,y_train)
-expected = y
-predicted = clf.predict(X)
-print(metrics.classification_report(expected, predicted))
-clf.score(X_test, y_test)
+#expected = y
+#predicted = clf.predict(X)
+#print(metrics.classification_report(expected, predicted))
+score = clf.score(X_test, y_test)
+print(r'准确率:',score)
+#print(predicted)
+testname = input('输入预测文件')
+preset = pd.read_csv(testname, header=None, sep=',')
+x = np.array(preset)
+predicted = clf.predict(x)
+x = pd.DataFrame(x)
+predicted = pd.DataFrame(predicted)
+res = pd.concat([x, predicted], axis=1)
+res.to_csv('res.csv',index=False,header=None)
